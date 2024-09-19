65fb076e715f2f5eff44d73a61ca5217c791c2bf
vegardwalland <vegard.walland@gmail.com>
2024-09-19 20:22:35 +0200

    update tideservice

--- src/main/java/com/vw/tide_eye/service/TideService.java
+++ src/main/java/com/vw/tide_eye/service/TideService.java
@@ -1,5 +1,7 @@
 package com.vw.tide_eye.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vw.tide_eye.exception.TideDataFetchException;
 import com.vw.tide_eye.model.SurgeData;
 import com.vw.tide_eye.model.Tide;
@@ -9,11 +11,14 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Service
 public class TideService {
@@ -64,9 +69,23 @@ public class TideService {
     }
 
     @Cacheable(value = "harbors", unless = "#result == null")
-    public String fetchHarbors() {
+    public List<String> fetchHarbors() throws TideDataFetchException {
         String url = "https://api.met.no/weatherapi/tidalwater/1.1/locations";
-        return restTemplate.getForObject(url, String.class);
+        String response = restTemplate.getForObject(url, String.class);
+
+        // Only need the harbor names, so parse the response to get them
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode root = mapper.readTree(response);
+            JsonNode featuresNode = root.path("features");
+
+            return featuresNode.findValues("title")
+                    .stream()
+                    .map(JsonNode::asText)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new TideDataFetchException("Error parsing harbor data", e);
+        }
     }
 
     private Tide parseToTide(String text, String harbor) throws TideDataFetchException {
@@ -82,21 +101,23 @@ public class TideService {
                 tide.setLastUpdated(DateTimeConverter.parseDateTime(parts[2] + " " + parts[3]));
             }
 
+            // Get data from the string, but not the header
             if (parts.length == 13 && !parts[0].equals("AAR")) {
                 try {
                     SurgeData surgeData = new SurgeData();
                     surgeData.setYear(Integer.parseInt(parts[0]));
                     surgeData.setMonth(Integer.parseInt(parts[1]));
                     surgeData.setDay(Integer.parseInt(parts[2]));
-                    surgeData.setPrognosis(Integer.parseInt(parts[4]));
+                    surgeData.setHour(Integer.parseInt(parts[3]));
+                    //surgeData.setPrognosis(Integer.parseInt(parts[4]));
                     surgeData.setSurge(Float.parseFloat(parts[5]));
                     surgeData.setTide(Float.parseFloat(parts[6]));
                     surgeData.setTotal(Float.parseFloat(parts[7]));
-                    surgeData.setSurgePercentage0(Float.parseFloat(parts[8]));
+                    /*surgeData.setSurgePercentage0(Float.parseFloat(parts[8]));
                     surgeData.setSurgePercentage25(Float.parseFloat(parts[9]));
                     surgeData.setSurgePercentage50(Float.parseFloat(parts[10]));
                     surgeData.setSurgePercentage75(Float.parseFloat(parts[11]));
-                    surgeData.setSurgePercentage100(Float.parseFloat(parts[12]));
+                    surgeData.setSurgePercentage100(Float.parseFloat(parts[12]));*/
                     surgeDataList.add(surgeData);
                 } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                     throw new TideDataFetchException("Error parsing surge data", e);
